cc72b2adbb7f617ec57e166dbf4388c9e3b8175b
mdfk15 <mtber@keemail.me>
2022-10-06 13:00:33 +0200

    Project modification & resources directory creation

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Forms CLI > GUI
 Project to development CLI application to GUI in Python with TKinter
+
+## Step 1
+Program to count courses per students
+  - CLI application
+  - Enter student and courses in list
+  - Option menu
+  - Option loop until "exit" option
+![First step Imge](resources/stp1.png)
+
+## Step 2
+Modification DB & add 3rd option.
+  - List to dictionary
+    - key: student name
+    - value: courses number
+  - 3rd option to single search
+![First step Imge](resources/stp2.png)
--- student-list.py
+++ student-list.py
@@ -3,8 +3,21 @@
 #  
 #  Copyright 2022 mdfk <mdfk@Elite>
 
-# Create empty list.
-students = []
+# Functions
+def verify(dato):
+	while dato == "":
+		print("Error")
+		dato = input("Try again: ")
+	return dato
+
+def convert(value):
+	while value.isdecimal() == False:
+		print("Error")
+		value = input("Try again: ")
+	return value
+
+# Create empty dicctionaire.
+students = {}
 
 # Main menu
 menu = """
@@ -12,7 +25,8 @@ menu = """
 ----------------------
 1 - Show student list
 2 - Enter student
-3 - Exit
+3 - Search student
+4 - Exit
 """
 
 # Menu loop (only exit with "3" option).
@@ -26,23 +40,26 @@ while True:
 			# I take each element in list students
 			# At the same time each element is a list
 			# The first element is the name and the second is the number of courses.
-			name = student[0]
-			courses = student[1]
+			courses = students[student]
 			# I need to define the varible "courses" 
 			# to concatenate it  with another variable or string
-			print(name + " - " + str(courses) + " courses")
+			print(student + " - " + str(courses) + " courses")
 	elif option == "2":
 		name_student = input("Enter the student name: ")
+		name_sturent = verify(name_student)
 		# Here we need to define it as "int" 
 		# since input() always return a "string"
-		courses = int(input("Enter number of courses: "))
-		if name_student == "":
-			print("Error: name no entered.")
-		else:
-			# Enter variables in list
-			students.append([name_student, courses])
-			print("Student successfully entered.")
+		courses = input("Enter number of courses: ")
+		courses = convert(courses)
+		students[name_student] = courses
+		print("Student successfully entered.")
 	elif option == "3":
+		name_student = input("Enter the student name: ")
+		if name_student in students:
+			print(name_student +" have "+ str(students[name_student])+" courses")
+		else:
+			print("Student does not exist in DB'")
+	elif option == "4":
 		# Break loop.
 		break
 	else:
