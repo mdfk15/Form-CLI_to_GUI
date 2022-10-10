771151457f14770873999ffe9abf83abd05efd31
mdfk15 <mtber@keemail.me>
2022-10-10 13:32:20 +0200

    Update step 3, modificate step 2 & delete etp1.png

--- README.md
+++ README.md
@@ -17,3 +17,9 @@ Modification DB & add 3rd option.
     - value: courses number
   - 3rd option to single search
 ![Second step Imge](resources/stp2.png)
+
+## Step 3
+Migrate project to Desktop GUI environment write in Tkinter
+  - Show student list in terminal
+  - Other option in GUI
+![Third step Image](resources/stp3.png)
--- student-list.py
+++ student-list.py
@@ -3,65 +3,85 @@
 #  
 #  Copyright 2022 mdfk <mdfk@Elite>
 
+# Libraries
+import tkinter as tk
+
 # Functions
 def verify(dato):
 	while dato == "":
-		print("Error")
-		dato = input("Try again: ")
+		dato = "Error"
 	return dato
 
 def convert(value):
 	while value.isdecimal() == False:
-		print("Error")
 		value = input("Try again: ")
 	return value
 
-# Create empty dicctionaire.
-students = {}
-
-# Main menu
-menu = """
-     Option menu
-----------------------
-1 - Show student list
-2 - Enter student
-3 - Search student
-4 - Exit
-"""
-
-# Menu loop (only exit with "3" option).
-while True:
-	print(menu)
-	
-	option = input("Enter the Option: ")
-	if option == "1":
-		print(f"\nStudent list:")
-		for student in students:
-			# I take each element in list students
-			# At the same time each element is a list
-			# The first element is the name and the second is the number of courses.
-			courses = students[student]
-			# I need to define the varible "courses" 
-			# to concatenate it  with another variable or string
-			print(student + " - " + str(courses) + " courses")
-	elif option == "2":
-		name_student = input("Enter the student name: ")
-		name_sturent = verify(name_student)
-		# Here we need to define it as "int" 
-		# since input() always return a "string"
-		courses = input("Enter number of courses: ")
-		courses = convert(courses)
-		students[name_student] = courses
-		print("Student successfully entered.")
-	elif option == "3":
-		name_student = input("Enter the student name: ")
-		if name_student in students:
-			print(name_student +" have "+ str(students[name_student])+" courses")
-		else:
-			print("Student does not exist in DB'")
-	elif option == "4":
-		# Break loop.
-		break
+def show_students(students):
+	if students:
+		print(f"\nStudents list: ")
+		for name,courses in students.items():
+			print(f"{name} - {courses} courses")
 	else:
-		print("Incorrect option, try again.")
-print("¡Thanks You for using this program!")
+		print("Student does not exist in DB")
+
+# TKinter function
+def clic(key):
+	if key == "list":
+		show_students(students)
+	elif key == "add":
+		name_student = verify(name.get())
+		curso_student = verify(courses.get())
+		if name_student == "Error":
+			print("Error")
+		else:
+			students[name_student] = curso_student
+			print("Student successfully entered.")
+	elif key == "courses":
+		name_student = verify(name.get())
+		print(name_student)
+		if name_student == "Error":
+			print("Error")
+		elif name_student in students:
+			print(f"{name_student} - {students[name_student]} courses")
+		else:
+			print("Student not found")
+
+# Data modeling.
+students = {"Romina":"5","Pedro":"3","Josefa":"13"}
+
+# Window para contener la calculadora
+window = tk.Tk()
+window.title("Courses form")
+window.config(width=350, height=290, bg="Light Steel Blue")
+window.resizable(0,0)
+
+label = tk.Label(text="Alumni - Python project", bg="Light Steel Blue")
+label.place(x=85,y=20)
+
+# Values
+width = 16
+height = 2
+
+# Botones
+button = tk.Button(text="Show studens list", width=width, height=height, command=lambda:clic("list"))
+button.place(x=15, y=70)
+
+button = tk.Button(text="Add student to list", width=width, height=height, command=lambda:clic("add"))
+button.place(x=15, y=220)
+
+button = tk.Button(text="Search student in list", width=width, height=height, command=lambda:clic("courses"))
+button.place(x=180, y=220)
+
+# Input
+name = tk.Entry()
+name.place(x=135,y=137,width=195,height=25)
+label = tk.Label(text="Nombre y apellido: ", bg="Light Steel Blue")
+label.place(x=15,y=140)
+
+courses = tk.Entry()
+courses.place(x=135,y=167,width=100,height=25)
+label = tk.Label(text="Cursos: ", bg="Light Steel Blue")
+label.place(x=15,y=170)
+
+window.mainloop()
